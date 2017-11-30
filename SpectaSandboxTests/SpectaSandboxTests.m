2726739172a08e5cb67bbafafd7444dd15789098
enomoto <kenta.enomoto@classi.jp>
2017-11-30 17:38:41 +0900

    add snapshot tests

--- SpectaSandboxTests/SpectaSandboxTests.m
+++ SpectaSandboxTests/SpectaSandboxTests.m
@@ -5,34 +5,25 @@
 //  Created by kenta.enomoto on 2017/11/30.
 //
 
-#import <XCTest/XCTest.h>
+#import "ViewController.h"
+#include <Specta/Specta.h>
+#include <Expecta/Expecta.h>
+#include <Expecta+Snapshots/EXPMatchers+FBSnapshotTest.h>
 
-@interface SpectaSandboxTests : XCTestCase
+SpecBegin(ViewController)
 
-@end
+__block ViewController *sut;
 
-@implementation SpectaSandboxTests
+describe(@"local", ^{
+    it (@"Initial view controller looks corrects", ^{
+        [sut view];
 
-- (void)setUp {
-    [super setUp];
-    // Put setup code here. This method is called before the invocation of each test method in the class.
-}
+        UIStoryboard *storyboard = [UIStoryboard storyboardWithName:@"Main" bundle:[NSBundle mainBundle]];
+        sut = [storyboard instantiateInitialViewController];
+//        expect(sut).to.recordSnapshot();
+        expect(sut).to.haveValidSnapshot();
+    });
 
-- (void)tearDown {
-    // Put teardown code here. This method is called after the invocation of each test method in the class.
-    [super tearDown];
-}
+});
 
-- (void)testExample {
-    // This is an example of a functional test case.
-    // Use XCTAssert and related functions to verify your tests produce the correct results.
-}
-
-- (void)testPerformanceExample {
-    // This is an example of a performance test case.
-    [self measureBlock:^{
-        // Put the code you want to measure the time of here.
-    }];
-}
-
-@end
+SpecEnd
